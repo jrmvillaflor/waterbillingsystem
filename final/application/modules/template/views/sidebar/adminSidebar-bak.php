c9fad36e43411c97fb6d9c6d9773493750a61d92
maykil <finalwater@gmail.com>
2020-06-29 02:54:07 +0800

    sidebar update

---
+++ final/application/modules/template/views/sidebar/adminSidebar-bak.php
@@ -0,0 +1,24 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-title ">ADMIN</li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('admin/adminDashboard');?>">
+                <i class="nav-icon icon-drop"></i> Admin Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('admin/permission');?>">
+                <i class="nav-icon icon-drop"></i> Customer Permission</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('admin/dashboards');?>">
+                <i class="nav-icon icon-drop"></i> Dashboards</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('admin/addEmployee');?>">
+                <i class="nav-icon icon-drop"></i>Add Employee</a>
+            </li>
+        </ul>
+    </nav>
+    
+</div>
--- final/application/modules/template/views/sidebar/cashierSidebar.php
+++ final/application/modules/template/views/sidebar/cashierSidebar.php
@@ -2,6 +2,12 @@
     <nav class="sidebar-nav">
         <ul class="nav">
             <li class="nav-title">CASHIER</li>
+            <?php if($this->session->user_type == 1):?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('admin/adminDashboard');?>">
+                    <i class="cil-wc"></i>Home</a>
+                </li>
+            <?php endif;?>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url('cashier/cashierDashboard');?>">
                 <i class="cil-money"></i> Cashier Dashboard</a>
