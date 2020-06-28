f81d0b9e0314d21970afa09293d15f479cb2057c
maykil <finalwater@gmail.com>
2020-06-29 02:16:24 +0800

    header update

--- final/application/modules/template/views/header.php
+++ final/application/modules/template/views/header.php
@@ -16,15 +16,19 @@
         <link rel="stylesheet" href="https://unpkg.com/@coreui/icons@1.0.0/css/flag.min.css">
         <!-- js -->
         <!-- <script src="<?php echo base_url('assets/js/main.js');?>"></script> -->
-        <!-- CHART -->
-        <!-- <script src="<?php echo base_url('assets/js/Chart/Chart.js');?>"></script>
-        <script src="<?php echo base_url('assets/js/Chart/samples/utils.js');?>"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/Chart/samples/style.css');?>"> -->
         
+        <!-- CHART -->
+        <script src="<?php echo base_url('assets/js/Chart/Chart.js');?>"></script>
+        <script src="<?php echo base_url('assets/js/Chart/samples/utils.js');?>"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/Chart/samples/style.css');?>">
+  
         <!-- jquery and jquery UI -->
         <link type="text/css" rel="stylesheet" href="<?php echo base_url("assets/main/jquery/jqueryUI/jquery-ui.css")?>" >
         <script type="text/javascript" src="<?php echo base_url("assets/main/jquery/jquery-3.5.1.min.js")?>"></script>
         <script type="text/javascript" src="<?php echo base_url("assets/main/jquery-ui.js")?>"></script>
 
+        <!-- CORE UI js -->
+        <script src="<?php echo base_url('assets/main/js/popper.min.js');?>"></script>
+        <script src="<?php echo base_url('assets/main/js/bootstrap.min.js');?>"></script>
 
     </head>
